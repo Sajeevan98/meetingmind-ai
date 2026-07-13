05bcebdcf4e41beb000b7abf034b720e0eeddbc6
Sajeevan98 <sajee91098@gmail.com>
2026-07-13 13:59:32 +0530

    chore: initialize project structure

---
+++ backend/src/test/java/com/sajee/meetingmind/MeetingmindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajee.meetingmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingmindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
