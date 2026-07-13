b8147edf370d84fcf2f566d33d466760714d9ce8
Sajeevan98 <sajee91098@gmail.com>
2026-07-13 21:44:07 +0530

    feat: create FileStorageService and Configuration properties

---
+++ backend/src/main/java/com/sajee/meetingmind/storage/config/FileStorageProperties.java
@@ -0,0 +1,19 @@
+package com.sajee.meetingmind.storage.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "app.file")
+public class FileStorageProperties {
+
+    private String uploadDir;
+
+    private Long maxSize;
+
+    private List<String> allowedTypes;
+}
