dac160864db439c4a7f8fdba1e66d7ceca620d5d
Sajeevan98 <sajee91098@gmail.com>
2026-07-14 17:09:30 +0530

    feat: create new exception class

---
+++ backend/src/main/java/com/sajee/meetingmind/util/exception/DocumentExtractionException.java
@@ -0,0 +1,9 @@
+package com.sajee.meetingmind.util.exception;
+
+public class DocumentExtractionException extends RuntimeException {
+
+    public DocumentExtractionException(String message, Throwable cause) {
+
+        super(message, cause);
+    }
+}
